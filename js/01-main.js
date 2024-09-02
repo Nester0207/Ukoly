62a6d18508cb231867092af855fbc787e0f2ed5e
Serhii Nesterchuk <s.nesterchuk27@gmail.com>
2024-09-02 19:42:57 +0200

    pes

---
+++ js/01-main.js
@@ -0,0 +1,21 @@
+let pes = {
+    jméno: "Pes",
+    plemeno: "anglicky kokspanel",
+    rokNarozeni: 2020,
+    hmotnosti: 15,
+    kohoutkovaVyska: 40,
+    srst: "mirne kaderava",
+    barvaSrsti: ["cerne", "bile", "zlate", "jatrove"],
+    oci: {
+        tvar: "kulaty",
+        barva: "tmavohnedy",
+    },
+    usi: "dlouhe",
+    povaha: "aktivni",
+    krmivo: {
+        stene: ["mleko", "tvaroh", "ovesna kase"],
+        dospely: ["varene brambory", "varene maso", "rzba", "drubez"]
+    }
+}
+
+console.log(pes);
